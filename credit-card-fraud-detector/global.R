0977d2ffab468c3e81fa022fcd4181d9df927844
Diwash shrestha <diwash712@gmail.com>
2019-06-29 13:37:22 +0545

    Add files via upload

--- credit-card-fraud-detector/global.R
+++ credit-card-fraud-detector/global.R
@@ -1,6 +1,7 @@
 library(shinydashboard)
 library(shinythemes)
 library(shinydashboardPlus)
+library(shinycssloaders)
 library(ggplot2)
 library(readr)
 library(dplyr)
@@ -10,14 +11,18 @@ library(h2o)
 library(highcharter)
 library(data.table)
 library(DT)
+library(lime)
 
 
-## create a dataframe to add the simulated real time data and use in visualization and infocards
-
+## Intialize the h2o
 h2o.init()
 
+## Load saved model
 model <- h2o.loadModel("/home/diwash/projeckt/credit_card_feaud/model/GBM_model_R_1558482610633_2")
-#test_df <- read_csv("/home/diwash/projeckt/credit_card_feaud/new_data.csv")
-#test.h2o <- as.h2o(test_df)
-
-#sample <- sample_frac(test_df, .1)
+#model <- h2o.loadModel("/home/diwash/projeckt/credit_card_feaud/credit-card-fraud-detector/model/DRF_model_R_1561701961587_2")
+modelrf <- h2o.loadModel("/home/diwash/projeckt/credit_card_feaud/credit-card-fraud-detector/model/DRF_model_R_1561701961587_2")
+modelxg <- h2o.loadModel("/home/diwash/projeckt/credit_card_feaud/credit-card-fraud-detector/model/XGBoost_model_R_1561701961587_884")
+modelgb <- h2o.loadModel("/home/diwash/projeckt/credit_card_feaud/credit-card-fraud-detector/model/GBM_model_R_1558482610633_2")
+## For LIME
+train <- fread("/home/diwash/projeckt/credit_card_feaud/train.csv")
+explainer <- lime(train, model, n_bins = 5)
--- credit-card-fraud-detector/ui.R
+++ credit-card-fraud-detector/ui.R
@@ -12,14 +12,19 @@ library(shiny)
 # Define UI for application that draws a histogram
 
 header <- dashboardHeader(
-  title = "ABC Bank Dashboard"
+  title = "ABC Bank "
 )
 
 
 sidebar <-  dashboardSidebar(
     sidebarMenu(
       menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-      menuItem("Alert", tabName = "alert", icon = icon("bell"))
+      menuItem("Alert", tabName = "alert", icon = icon("bell")),
+      menuItem("Alerts", tabName = "rfalert", icon = icon("bell"),
+               collapsible = TRUE,
+               menuSubItem("Random Forest",tabName = "rfalert"),
+               menuSubItem("XGBoost", tabName = "xgalert"),
+               menuSubItem("GBM", tabName = "gbmalert"))
     )
   )
 
@@ -27,7 +32,10 @@ body <-  dashboardBody(
     tabItems(
       source("ui/01-dashboard-ui.R", local= TRUE)$value,
     # Second tab content
-    source("ui/02-alert-ui.R", local= TRUE)$value
+    source("ui/02-alert-ui.R", local= TRUE)$value,
+    source("ui/02-rfalert-ui.R", local= TRUE)$value,
+    source("ui/02-xgalert-ui.R", local= TRUE)$value,
+    source("ui/02-gbmalert-ui.R", local= TRUE)$value
     )
   )
 
